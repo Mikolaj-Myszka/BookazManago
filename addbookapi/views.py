1c1a0e27f1abcea2275fa9632a5f5b530856a681
Miko <zito6z@gmail.com>
2019-05-22 09:21:58 +0200

    Added saving api results

--- addbookapi/views.py
+++ addbookapi/views.py
@@ -1,32 +1,62 @@
 from django.shortcuts import render
 import requests
 
+from books.models import Book
 
 def addbookapi(request):
-    text = 'some text'
-    url = 'https://www.googleapis.com/books/v1/volumes?q={}'
-    keyword = 'Hobbit'
-    
-    r = requests.get(url.format(keyword)).json()
-    #print(r.text)
 
-    book_fields = {
-        'title': r['items'][0]['volumeInfo']['title'],
-        'author': r['items'][0]['volumeInfo']['authors'][0],
-        'description': 'Some description here', #r['items'][0]['volumeInfo']['description'],
-        'category': r['items'][0]['volumeInfo']['categories'][0]
+    context = {}
+
+    if request.method == "POST":
+        keywords = request.POST.get('keywords')
+        print(keywords)
+
+        
+        url = 'https://www.googleapis.com/books/v1/volumes?q={}'
+        #keywords = 'Hobbit'
+        
+        r = requests.get(url.format(keywords)).json()
+        #print(r.text)
+
+        if 'title' in r['items'][0]['volumeInfo']:
+            title = r['items'][0]['volumeInfo']['title']
+            print("Title is: " + title)
+        else:
+            title = "No title"
+
+        if 'authors' in r['items'][0]['volumeInfo']:
+            author = r['items'][0]['volumeInfo']['authors'][0]
+            print("author is: " + author)
+        else:
+            author = "No author"
+
+        if 'description' in r['items'][0]['volumeInfo']:
+            description = r['items'][0]['volumeInfo']['description']
+            print("description is: " + description)
+        else:
+            description = "No description"
+
+        if 'categories' in r['items'][0]['volumeInfo']:
+            category = r['items'][0]['volumeInfo']['categories'][0]
+            print("category is: " + category)
+        else:
+            category = "No category"
+
+        
+        book_fields = {
+            'title': title,
+            'author': author,
+            'description': description[0:300] + '...', #r['items'][0]['volumeInfo']['description'],
+            'category': category
+            }
+        print(book_fields)
+
+        Book.objects.create(**book_fields)
+        
+        context = {
+            'book_fields': book_fields
         }
-    print(book_fields)
-    
-    """
-    form = BookForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = BookForm()
-    """
-    context = {
-        'book_fields': book_fields
-    }
-    
+        
+
 
     return render(request, 'addbookapi/addbookapi.html', context)
